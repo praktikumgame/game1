623c2441b5cdd161741652ed97dcac23b2099882
Voytenko Anton <voytenko.ad@yandex.ru>
2020-09-07 12:04:58 +0800

    Button is refactored

--- src/components/Common/Button/types.ts
+++ src/components/Common/Button/types.ts
@@ -2,4 +2,5 @@ export interface IProps {
   onClick: (event: React.MouseEvent) => void;
   formIsValid?: boolean;
   buttonText: string;
+  className?: string;
 }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -37,7 +37,7 @@ const Form = ({
         </div>
       )}
       {serverError && <p className="form__server-error">{serverError}</p>}
-      <Button onClick={onClick} formIsValid={!formIsValid} buttonText={buttonText} />
+      <Button className="form__button" onClick={onClick} formIsValid={!formIsValid} buttonText={buttonText} />
     </form>
   );
 };
