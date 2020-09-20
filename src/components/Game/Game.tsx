4f0f3c415747c19830bc109a133690fca482f41c
Voytenko Anton <voytenko.ad@yandex.ru>
2020-09-20 16:14:54 +0800

    Fixed typo in inmport path

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { withErrorBoundary } from '../';
+import { withErrorBoundary } from '../ErrorBoundary/ErrorBoundary';
 
 import './Game.css';
 
-export const Game = () => {
-  return <h1 className="game">Welcome to GAME</h1>;
-};
+const Game = () => <h1 className="game">Welcome to GAME</h1>;
 
 const GameWithErrorHandler = withErrorBoundary(Game);
 
