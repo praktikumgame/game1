d148863edd9565c652156c478d1f86177208cbd6
Anton Voytenko <voytenko.ad@yandex.ru>
2020-09-28 00:00:56 +0800

    Rebuilded auth directory

---
+++ src/services/auth/helpers/withPreload.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { Preloader } from '../../../components';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkAuthorize } from '../../../redux/actions';
+import { withAuth } from './withAuth';
+import { withAuthProps } from '../types';
+import { IAppState } from '../../../redux/appReducer';
+
+const withPreload = <T,>(WrappedComponent: React.ComponentType<T>) => {
+  const withPreloadComponent = withAuth((props: T & withAuthProps) => {
+    const dispatch = useDispatch();
+    const appIsInit = useSelector((state: { app: IAppState }) => state.app.initApp);
+
+    useEffect(() => {
+      dispatch(checkAuthorize());
+    }, []);
+
+    if (!props.isAuthorized && !appIsInit) {
+      return <Preloader />;
+    }
+    return <WrappedComponent {...props} />;
+  });
+
+  return withPreloadComponent;
+};
+
+export { withPreload };
---
+++ src/services/auth/types.ts
@@ -0,0 +1,1 @@
+export type withAuthProps = { isAuthorized: boolean };
