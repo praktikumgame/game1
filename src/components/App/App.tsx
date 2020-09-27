03a9984abd9d5e50078e0982c1a5d83c219072d0
Anton Voytenko <voytenko.ad@yandex.ru>
2020-09-27 22:18:53 +0800

    Deleted authProvider, this old topic;)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,25 +11,22 @@ import {
   NotFound,
   Leaderboard,
 } from '../';
-import { AuthProvider } from '../../services/Auth';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app">
-      <AuthProvider>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/signin" component={Signin} />
-          <PrivateRoute path="/settings" component={Settings} />
-          <PrivateRoute path="/game" component={GameWithErrorHandler} />
-          <PrivateRoute path="/leaderboard" component={Leaderboard} />
-          <Route path="/notfound" component={NotFound} />
-          <Redirect to="/notfound" />
-        </Switch>
-      </AuthProvider>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
+        <PrivateRoute path="/settings" component={Settings} />
+        <Route path="/game" component={GameWithErrorHandler} />
+        <PrivateRoute path="/leaderboard" component={Leaderboard} />
+        <Route path="/notfound" component={NotFound} />
+        <Redirect to="/notfound" />
+      </Switch>
     </div>
   );
 };
