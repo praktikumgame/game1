8e848814db94503e82d0f4bf4e6d02744170ee5c
Anton Voytenko <voytenko.ad@yandex.ru>
2020-10-01 17:30:57 +0800

    Eliminated unnecessary changes in Settings

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -33,7 +33,7 @@ const Settings = () => {
       return;
     }
 
-    if (!isMatch) {
+    if (!isMatch && changePasswordError) {
       dispatch(changePasswordClearError());
     }
   }, [values]);
