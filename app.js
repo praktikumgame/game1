9ee3f91483242e277fc30fe7d6af4a0246443a7f
Volodya262 <volodya_26@mail.ru>
2020-12-27 19:48:51 +0500

    Вкрутил docker

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
+const host = '0.0.0.0';
 
 app.use(express.static(`${__dirname}/dist`));
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
 });
 
-app.listen(port);
+app.listen(port, host, () => {
+  console.log(`Приложение доступно по адресу: http://localhost:${port}!`);
+});
